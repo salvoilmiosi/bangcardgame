fc25dbdade59d5f9808a9677508429e4882992dc
Salvo Miosi <salvo.ilmiosi@gmail.com>
2020-06-15 03:17:15 +0200

    aggiornati asset

---
+++ Assets/BangDeck.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BangDeck : MonoBehaviour
+{
+    public GameObject cardPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CardLoader.loadCards();
+
+        List<Card> shuffledCards = new List<Card>(CardLoader.deckBang);
+        DeckShuffler.Shuffle(shuffledCards);
+
+        float z = 0.0f;
+        foreach (Card card in shuffledCards) {
+            GameObject obj = Instantiate(cardPrefab);
+            obj.transform.parent = transform;
+            obj.transform.position = new Vector3(0.0f, 0.0f, z);
+            CardSprite cardSprite = obj.GetComponent<CardSprite>();
+            cardSprite.card = card;
+            z += 0.015f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/DeckShuffler.cs
+++ Assets/DeckShuffler.cs
@@ -13,7 +13,7 @@ public class DeckShuffler : MonoBehaviour
         int n = list.Count;  
         while (n > 1) {  
             n--;  
-            int k = (int)(Random.value * n);  
+            int k = Random.Range(0, n);
             T value = list[k];
             list[k] = list[n];
             list[n] = value;
